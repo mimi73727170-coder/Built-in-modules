43a09892bf67f5926d355e7fe6fc0ec732c4a5f1
Mimi <mimi73727170@gmail.com>
2026-06-17 01:49:01 +0530

    Add files via upload

---
+++ Built-in modules.py
@@ -0,0 +1,22 @@
+import math 
+print(math.sqrt(25))
+print(math.pi)
+
+import random 
+print(random.randint(1,10))
+
+from datetime import datetime
+now = datetime.now()
+print(now)
+
+import math
+print(math.sqrt(81))
+print(math.pi)
+
+import random 
+print(random.randint(1,100))
+
+from datetime import datetime
+now = datetime.now()
+print(now)
+
---
+++ Lucky Number Generator.py
@@ -0,0 +1,12 @@
+import random 
+
+name = input("Enter your name :")
+
+print("Hello",name)
+
+print("Your lucky number is :",random.randint(1,100))
+
+
+
+
+
